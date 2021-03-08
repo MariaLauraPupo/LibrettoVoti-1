447a71f91f1f77c2107707632325251196fdded8
Hp <Hp@LAPTOP-N2LMCUK2.station>
2021-03-08 17:25:46 +0100

    soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -12,6 +12,41 @@ public class Libretto {
 	public void add(Voto v) {
 		voti.add(v);
 	}
+	/*metodi per stampare i voti uguali a 25*/
+	//1
+	/*public void stampaVotiUguali(int punteggio) {
+		//sa fare solo
+		System.out.println();
+	}
+	//2
+	public String votiUguali(int punteggio) {
+		//calcola una stringa che contiene i voti
+		//sarà poi il richiamante a stamparli
+		//più generale
+		//come comporre la stringa?
+	}*/
+	//3
+	public List<Voto> listaVotiUguali(int punteggio){
+		//restituisce solo i voti uguali al criterio
+		List<Voto> risultato=new ArrayList<>();
+		for(Voto v:this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v);
+			}
+		}
+		return risultato;
+		
+	}
+	//4 TODO
+	public Libretto votiUguali3(int punteggio) {
+		//restituisco il libretto formato da quei determinati voti, così ha tutte le funzionalità del libretto
+	Libretto risultato = new Libretto();
+	for(Voto v:this.voti) {
+		if(v.getVoto()==punteggio) {
+			risultato.add(v);
+		}
+	}return risultato;
+	}
 	public String toString() {
 		String s ="";
 		for(Voto v:this.voti) {
--- src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
@@ -1,6 +1,8 @@
 package it.polito.tdp.librettovoti.model;
 
+
 import java.time.LocalDate;
+import java.util.List;
 
 //classe con il metodo main
 public class TestLibretto {
@@ -15,8 +17,18 @@ public class TestLibretto {
 	libretto.add(voto1);
 	libretto.add(new Voto("Fisica1", 27, LocalDate.of(2019,7,12)));
 	libretto.add(new Voto("Informatica", 18, LocalDate.of(2019,9,8)));
+	libretto.add(new Voto("Chimica", 25, LocalDate.of(2019,7,6)));
 	
-	System.out.println(libretto);
 
+	System.out.println(libretto);
+	
+	//utilizzando il metodo 3
+	List<Voto> venticinque = libretto.listaVotiUguali(25);
+	System.out.println(venticinque);
+	
+	//utilizzando il metodo 4
+	Libretto librettoventicinque = libretto.votiUguali3(25);
+	System.out.println(librettoventicinque);
+ 
 }
 }
